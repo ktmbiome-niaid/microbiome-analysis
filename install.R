c68cfa513a31937b58812a475ef24b1796f98f2d
Kathryn McCauley <kathryn.mccauley@nih.gov>
2024-06-26 15:18:40 -0700

    Remove complex packages

--- install.R
+++ install.R
@@ -4,19 +4,8 @@ install.packages("vegan")
 install.packages("ape")
 install.packages("ggrepel")
 install.packages("picante")
-install.packages("igraph")
 
 install.packages("BiocManager")
 
 BiocManager::install("phyloseq")
 BiocManager::install("Maaslin2")
-
-install.packages("devtools")
-devtools::install_github("zdk123/SpiecEasi")
-
-## If you have trouble installing the above package on your computer, try running the following commands in Terminal:
-##    mkdir -p $HOME/.R
-##    echo "FLIBS=-L/opt/local/lib/gcc48/" > $HOME/.R/Makevars
-
-## I initially ran into some errors relating to gfortran and this resolves the issue.
-## These commands were found here: https://github.com/zdk123/SpiecEasi/issues/217#issuecomment-1489172072
