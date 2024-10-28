269305464e3f244af39d0e30bc73c254f5913e31
ktmbiome-niaid <kathryn.mccauley@nih.gov>
2024-10-28 11:59:55 -0700

    Preinstall glmmTMB

--- install.R
+++ install.R
@@ -3,12 +3,10 @@ install.packages("vegan")
 install.packages("ape")
 install.packages("ggrepel")
 install.packages("picante")
+# Try pre-installing?
+install.packages("glmmTMB")
 
 install.packages("BiocManager")
 
 BiocManager::install("phyloseq")
-#BiocManager::install("Maaslin2")
-## Trouble installing glmmTMB, installing github development release:
-install.packages("devtools")
-devtools::install_github("biobakery/Maaslin2")
-## If this doesn't work, consider installing dependencies of Maaslin2 (like glmmTMB) before installing Maaslin2? Or create a new repo and only install glmmTMB?
+BiocManager::install("Maaslin2")
