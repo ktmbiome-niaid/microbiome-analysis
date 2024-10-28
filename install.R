9b83a5ec1b3d56ca8403f2bf329b57c45c5e6ae3
ktmbiome-niaid <kathryn.mccauley@nih.gov>
2024-10-28 08:39:10 -0700

    Add install file

---
+++ install.R
@@ -0,0 +1,12 @@
+install.packages("rmarkdown")
+install.packages("ggplot2")
+install.packages("vegan")
+install.packages("ape")
+install.packages("ggrepel")
+install.packages("picante")
+
+install.packages("BiocManager")
+
+BiocManager::install("phyloseq")
+BiocManager::install("Maaslin2")
+
