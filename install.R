1a0a90015d82d641d687889f8b2b602089de68fe
ktmbiome-niaid <kathryn.mccauley@nih.gov>
2024-10-28 12:32:12 -0700

    Only Install glmmTMB

--- install.R
+++ install.R
@@ -1,12 +1,1 @@
-install.packages("ggplot2")
-install.packages("vegan")
-install.packages("ape")
-install.packages("ggrepel")
-install.packages("picante")
-# Try pre-installing?
 install.packages("glmmTMB")
-
-install.packages("BiocManager")
-
-BiocManager::install("phyloseq")
-BiocManager::install("Maaslin2")
